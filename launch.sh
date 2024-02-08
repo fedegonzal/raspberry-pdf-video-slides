7b9713d8251f6cc1210085100c785b7821fcf321
Federico Gonzalez Brizzio <fedegonzal@gmail.com>
2024-02-08 12:07:20 +0100

    we are not using size param

--- launch.sh
+++ launch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-chromium-browser "http://localhost:8080" &
+chromium --start-fullscreen --enable-chrome-browser-cloud-management --autoplay-policy=no-user-gesture-required "http://localhost:8080/video-fullscreen.html"
+# https://fedegonzal.github.io/raspberry-pdf-video-slides/
 
-sleep 2          # Wait for 2 seconds before the next click
-xdotool key 's'
+#sleep 2          # Wait for 2 seconds before the next click
+#xdotool key 's'
 
 #while true; do
 #    sleep 2          # Wait for 2 seconds before the next click
